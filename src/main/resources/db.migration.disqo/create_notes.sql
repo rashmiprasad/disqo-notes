5bdbf87089258d7b3a415599d79a9f7bd4e0dca4
Rashmi Prasad <rprasad@insikt.com>
2020-05-22 21:18:25 -0700

    Updating SQL scripts for Note and User entity.

--- src/main/resources/db.migration.disqo/create_notes.sql
+++ src/main/resources/db.migration.disqo/create_notes.sql
@@ -1,8 +1,8 @@
  create table disqo.note (
-    id serial PRIMARY KEY,
+    id BigSerial PRIMARY KEY,
     user_id  integer NOT NULL,
     title VARCHAR (50) UNIQUE NOT NULL,
-    note VARCHAR (1000) UNIQUE NOT NULL,
+    note VARCHAR (1000) ,
     created_date_time TIMESTAMP  default current_timestamp,
     updated_date_time TIMESTAMP  default current_timestamp
-    );
+    )
--- src/main/resources/db.migration.disqo/create_user.sql
+++ src/main/resources/db.migration.disqo/create_user.sql
@@ -1,5 +1,5 @@
 create table disqo.user (
-    id serial PRIMARY KEY,
+    id BigSerial PRIMARY KEY,
     email VARCHAR (50) UNIQUE NOT NULL,
     password VARCHAR (50) NOT NULL,
     created_date_time TIMESTAMP  default current_timestamp,
